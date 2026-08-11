f40d75f7335fad2d351c942aa0064cd6f2965445
nahyun <chlskgus789@gmil>
2026-08-11 18:59:38 +0900

    Feat: main.py 진입점 및 메뉴 화면 출력

---
+++ main.py
@@ -0,0 +1,19 @@
+def print_menu():
+    """메뉴 화면을 출력한다."""
+    print("="*20)
+    print("🎯 넌센스 퀴즈 게임 🎯")
+    print("="*20)
+    print("1. 퀴즈 풀기")
+    print("2. 퀴즈 추가")
+    print("3. 퀴즈 목록")
+    print("4. 점수 확인")
+    print("5. 종료")
+    print("="*20)
+
+
+def main():
+    print_menu()
+
+
+if __name__ == "__main__":
+    main()
